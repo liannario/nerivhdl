f6c31a937c33031d47d94c1f3314fec7e626d54c
lucaiannario <lucaiannario@60184080-e597-11de-92aa-3ddf338522b3>
2010-01-14 13:10:28 +0000

    implementato modulo btb

--- trunk/Btb_component.vhd
+++ trunk/Btb_component.vhd
@@ -64,7 +64,130 @@ begin
 		
 	end process;
 	
+	a_read : process(rd, pc_if)
+		
+	variable tag : std_logic_vector(TAG_BITS-1 downto 0);
+	variable index : integer;
+	variable found : std_logic;
+	variable found_index : integer := 0;
 	
+	begin
+	
+		if(rd = '1') then
+			tag := pc_if(PC_BITS-1 downto SLOT_BITS);
+			index := conv_integer(pc_if(SLOT_BITS-1 downto 0));
+			found := '0';
+			
+			-- ricerca della linea di cache
+			for i in 0 to SLOTS_NUM-1 loop
+				if(Btb_inst(index)(i).tag_pc = tag and Btb_inst(index)(i).status = '1') then -- linea trovata e valida	
+					found := '1';
+					found_index := i;				
+					pc_dest_if <= Btb_inst(index)(i).dest_pc; -- verificare assegnamento di tutti i bit. aggiorno pc_dest_if --downto				 
+					case Btb_inst(index)(i).pred is -- emetto il bit di predizione
+						when "11" => tkn_if <= '1';
+						when "10" => tkn_if <= '1';
+						when others => tkn_if <= '0'; 
+					end case;					
+					Btb_inst(index)(i).repl <= '0';
+				end if;
+			end loop;
+			
+			-- gestione della politica di rimpiazzamento
+			if(found = '1') then -- la linea è stata trovata	
+				for i in 0 to SLOTS_NUM-1 loop
+					if(i /= found_index) then 			
+						Btb_inst(index)(i).repl <= '1';		
+					end if;
+				end loop;
+			else
+				tkn_if <= '0'; -- anche nel caso non venga trovata la linea corrispondente
+			end if;
+		end if;
+		
+	end process;
+	
+	a_write : process(wr)
+	
+	variable tag : std_logic_vector(TAG_BITS-1 downto 0);
+	variable index : integer;
+	variable found : std_logic;
+	variable found_invalid : std_logic;
+	variable found_invalid_index : integer := 0;
+	
+	begin
+	
+	if(wr = '1') then
+			tag := pc_ex(PC_BITS-1 downto SLOT_BITS);
+			index := conv_integer(pc_ex(SLOT_BITS-1 downto 0));
+			found := '0';
+			found_invalid := '0';
+			
+			for i in 0 to SLOTS_NUM-1 loop
+				if(Btb_inst(index)(i).tag_pc = tag and Btb_inst(index)(i).status = '1') then -- il controllo della validità potrebbe essere omesso
+					found := '1';
+					Btb_inst(index)(i).dest_pc <= pc_dest_ex; -- aggiorno indirizzo di destinazione --downto
+					
+					--aggiornamento bit di predizione
+					if(pred_ok_ex = '1') then
+						case Btb_inst(index)(i).pred is
+							when "11" => Btb_inst(index)(i).pred <= "11";
+							when "10" => Btb_inst(index)(i).pred <= "11";
+							when "01" => Btb_inst(index)(i).pred <= "00";
+							when "00" => Btb_inst(index)(i).pred <= "00";
+							when others => Btb_inst(index)(i).pred <= "00";
+						end case;
+					else
+						case Btb_inst(index)(i).pred is
+							when "11" => Btb_inst(index)(i).pred <= "10";
+							when "10" => Btb_inst(index)(i).pred <= "01";
+							when "01" => Btb_inst(index)(i).pred <= "10";
+							when "00" => Btb_inst(index)(i).pred <= "01";		
+							when others => Btb_inst(index)(i).pred <= "00";
+						end case;
+					end if;
+				end if;
+			end loop;
+			
+			if(found = '0') then --linea non trovata -> scelgo la linea da rimpiazzare
+				for i in 0 to SLOTS_NUM-1 loop
+					if(Btb_inst(index)(i).status = '0') then -- trovata linea invalida
+						found_invalid := '1';
+						found_invalid_index := i;
+						Btb_inst(index)(i).tag_pc <= tag; --downto
+						Btb_inst(index)(i).dest_pc <= pc_dest_ex; --downto
+						Btb_inst(index)(i).status <= '1';
+						Btb_inst(index)(i).repl <= '0';
+						if(pred_ok_ex = '1') then
+							Btb_inst(index)(i).pred <= "00";
+						else
+							Btb_inst(index)(i).pred <= "11";
+						end if;
+						exit; --break
+					end if;
+				end loop;
+				
+				if(found_invalid = '1') then
+					for i in 0 to SLOTS_NUM-1 loop
+						if(i /= found_invalid_index) then
+							Btb_inst(index)(i).repl <= '1';
+						end if;
+					end loop;
+				else -- non ci sono linee invalide
+					for i in 0 to SLOTS_NUM-1 loop
+						if(Btb_inst(index)(i).repl = '1') then -- trovata linea da rimpiazzare
+							Btb_inst(index)(i).tag_pc <= tag; --downto
+							Btb_inst(index)(i).dest_pc <= pc_dest_ex; --downto
+							Btb_inst(index)(i).repl <= '0';
+						else
+							Btb_inst(index)(i).repl <= '1';
+						end if;
+					end loop;
+				end if;
+			end if;
+	end if;
+	
+	end process;
 	
 end Behavioral;
 
